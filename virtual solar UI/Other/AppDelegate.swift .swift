ba75e4a0a217f608f1f3403695b4021181304afd
Abubakar Abbas <abubakarabbas790@gmail.com>
2025-04-17 04:53:28 +1000

    backend updated

--- virtual solar UI/Other/AppDelegate.swift .swift
+++ virtual solar UI/Other/AppDelegate.swift .swift
@@ -5,4 +5,25 @@
 //  Created by Abubakar Abbas on 17/4/2025.
 //
 
-import Foundation
+import UIKit
+import Firebase
+import FacebookCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
+    ) -> Bool {
+        ApplicationDelegate.shared.application(
+            application,
+            didFinishLaunchingWithOptions: launchOptions
+        )
+        return true
+    }
+
+    func application(_ app: UIApplication,
+                     open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return ApplicationDelegate.shared.application(app, open: url, options: options)
+    }
+}
--- virtual solar UI/Other/virtual_solar_UIApp.swift
+++ virtual solar UI/Other/virtual_solar_UIApp.swift
@@ -1,14 +1,17 @@
-//
-//  virtual_solar_UIApp.swift
-//  virtual solar UI
-//
-//  Created by Lachlan Jiang on 27/2/2025.
-//
-
 import SwiftUI
+import Firebase
+import FacebookCore
 
 @main
 struct virtual_solar_UIApp: App {
+    // ✅ Connect to the AppDelegate
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+    init() {
+        // ✅ Initialize Firebase
+        FirebaseApp.configure()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
